3938b76bcc1afff4d7eff63162971e2c8970e6d2
Michael Lang <michael.lang@nexul.com>
2018-01-13 14:57:26 -0600

    Step004: create the customer model definitions

---
+++ simple-crm-cli/src/app/customer/customer.model.ts
@@ -0,0 +1,14 @@
+// Allows custom status codes to be defined
+
+export type InteractionMethod = 'phone' | 'email';
+
+export interface Customer {
+  customerId: number;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  emailAddress: string;
+  preferredContactMethod: InteractionMethod;
+  statusCode: string;
+  lastContactDate: string; // ISO format date
+}
