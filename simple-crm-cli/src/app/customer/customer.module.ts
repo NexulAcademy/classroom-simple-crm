adedc94c186ae1919caea51eb1dee7614d402a13
Michael Lang <michael.lang@nexul.com>
2018-01-13 16:48:20 -0600

    Step005: customer list table on a card, with a max content width

--- simple-crm-cli/src/app/customer/customer.module.ts
+++ simple-crm-cli/src/app/customer/customer.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
+import { SharedImportsModule } from '../shared/shared-imports.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    SharedImportsModule
   ],
   declarations: [
     CustomerListPageComponent
