40a03ffdc9a477bc6db24264df620cb25c843411
Michael Lang <michael.lang@nexul.com>
2018-01-24 20:48:54 -0600

    Step009:building the customer add dialog

--- simple-crm-cli/src/app/customer/customer-list-page/customer-list-page.component.ts
+++ simple-crm-cli/src/app/customer/customer-list-page/customer-list-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from '../customer.model';
-import { MatTableDataSource } from '@angular/material';
 import { CustomerService } from '../customer.service';
 import { SimpleDataSource } from '../../shared/simple-datasource';
 import { Observable } from 'rxjs/Observable';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { CustomerCreateDialogComponent } from '../customer-create-dialog/customer-create-dialog.component';
 
 @Component({
   selector: 'crm-customer-list-page',
@@ -15,10 +16,29 @@ export class CustomerListPageComponent implements OnInit {
   dataSource: SimpleDataSource<Customer>;
   displayColumns = ['name', 'phone', 'email', 'status'];
 
-  constructor(private customerService: CustomerService) {}
+  constructor(
+    private customerService: CustomerService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
+    this.search();
+  }
+  private search() {
     this.customers$ = this.customerService.search('');
     this.dataSource = new SimpleDataSource(this.customers$);
+  }
+
+  addCustomer() {
+    const dialogRef = this.dialog.open(CustomerCreateDialogComponent, {
+      width: '250px',
+      data: null
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.customerService.save(result);
+      this.search();
+    });
   }
 }
--- simple-crm-cli/src/app/customer/customer.module.ts
+++ simple-crm-cli/src/app/customer/customer.module.ts
@@ -8,6 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { CustomerService } from './customer.service';
 import { CustomerMockService } from './customer-mock.service';
 import { environment } from '../../environments/environment';
+import { CustomerCreateDialogComponent } from './customer-create-dialog/customer-create-dialog.component';
 
 @NgModule({
   imports: [
@@ -17,13 +18,17 @@ import { environment } from '../../environments/environment';
     SharedImportsModule
   ],
   declarations: [
-    CustomerListPageComponent
+    CustomerListPageComponent,
+    CustomerCreateDialogComponent
   ],
   providers: [
     {
       provide: CustomerService,
       useClass: environment.production ? CustomerService : CustomerMockService
     }
+  ],
+  entryComponents: [
+    CustomerCreateDialogComponent
   ]
 })
 export class CustomerModule { }
