0068eb862a0577a7b9a6ad7778c2de378bec809a
Michael Lang <michael.lang@nexul.com>
2018-02-19 11:07:36 -0600

    Step019: unit testing app component

--- simple-crm-cli/src/app/app.component.spec.ts
+++ simple-crm-cli/src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { SharedImportsModule } from './shared/shared-imports.module';
+fdescribe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        SharedImportsModule
       ],
       declarations: [
         AppComponent
@@ -22,10 +28,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('crm');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in the toolbar', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to crm!');
+    expect(compiled.querySelector('.mat-toolbar ').textContent).toContain('Simple CRM');
   }));
 });
