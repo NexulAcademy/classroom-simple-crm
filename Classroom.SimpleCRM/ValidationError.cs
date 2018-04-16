40124b8af5db7dbd9bf94425bb4540d78c4609a3
Michael Lang <michael.lang@nexul.com>
2018-04-15 20:25:15 -0500

    Step112: validation model

---
+++ Classroom.SimpleCRM/ValidationError.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classroom.SimpleCRM
+{
+    public class ValidationError
+    {
+        public string Field { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Classroom.SimpleCRM/ValidationState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classroom.SimpleCRM
+{
+    /// <summary>
+    /// A container for validation errors used in business layer logic.
+    /// Useful to return results from a workflow or errors based on status of back-end data
+    /// or user record ownership.
+    /// </summary>
+    public class ValidationState
+    {
+        public List<ValidationError> Errors { get; set; }
+        public ValidationState()
+        {
+            Errors = new List<ValidationError>();
+        }
+    }
+}
