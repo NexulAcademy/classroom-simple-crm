f42248e130c1e1a025ceb29a200356d52d6f858c
Michael Lang <michael.lang@nexul.com>
2018-12-29 09:57:05 -0600

    Step 128: get user data with JWT for authenticated user

---
+++ Classroom.SimpleCRM.WebApi/Models/UserSummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Classroom.SimpleCRM.WebApi.Models
+{
+    public class UserSummaryViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string JwtToken { get; set; }
+        public string[] Roles { get; set; }
+        //TODO: add other app specific capabilities for the user
+        // one option...
+        public int AccountId { get; set; }
+    }
+}
